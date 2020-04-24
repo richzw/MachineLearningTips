d1713c0d1b3744288a8157cb603a733f57ee3404
臧伟 <richzw@gmail.com>
2020-04-24 20:22:00 +0800

    Create Curve_fit.py

---
+++ sklearn/Curve_fit.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+# generate some data with noise
+xData = np.linspace(0.01, 100., 50)
+aOrg = 0.08
+Norg = 10.5
+yData = Norg * xData ** (-aOrg) + np.random.normal(0, 0.5, len(xData))
+
+# get logarithmic data
+lx = np.log(xData)
+ly = np.log(yData)
+
+def f(x, N, a):
+    return N * x ** (-a)
+
+def f_log(x, lN, a):
+    return a * x + lN
+
+# optimize using the appropriate bounds
+popt, pcov = curve_fit(f, xData, yData, bounds=(0, [30., 20.]))
+popt_log, pcov_log = curve_fit(f_log, lx, ly, bounds=([0, -10], [30., 20.]))
+
+xnew = np.linspace(0.01, 100., 5000)
+
+# plot the data
+plt.plot(xData, yData, 'bo')
+plt.plot(xnew, f(xnew, *popt), 'r')
+plt.plot(xnew, f(xnew, np.exp(popt_log[0]), -popt_log[1]), 'k')
+plt.show()
